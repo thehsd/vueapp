8632792cef65329f4ffce4a68d3c3173ecbf84ce
hsdalireza <hosseinialireza485@gmail.com>
2020-11-02 22:57:13 +0330

    api

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import vueResource from 'vue-resource'
 import App from './App.vue'
 
+Vue.use(vueResource)
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+ 
